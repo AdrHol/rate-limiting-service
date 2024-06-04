644ae97ebb445929fb144ac26b926a06a02ccbfa
Adrian Holubecki <adrian.holubecki@capgemini.com>
2024-06-04 20:55:53 +0200

    Initial commit

---
+++ src/test/java/com/adrhol/rate_limiting_service/RateLimitingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrhol.rate_limiting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
